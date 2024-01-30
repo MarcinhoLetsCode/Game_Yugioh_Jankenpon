ee57ff722fe877acce7be3123ce44c68d5a48226
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2024-01-30 16:38:10 -0300

    Limpeza de Campo + Delete Carta Usada

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -13,16 +13,22 @@ const state = {
         player: document.getElementById("player-field-card"),
         computer: document.getElementById("computer-field-card"),
     },
+    playerSides: {
+        player1: "player-cards",
+        player1BOX: document.querySelector("#player-cards"),
+        computer: "computer-cards",
+        computerBOX: document.querySelector("#computer-cards"),
+    },
     actions: {
         button: document.getElementById("next-duel"),
         init: document.getElementById("start-duel"),
     },
 };
 
-const playerSides = {
-    player1: "player-cards",
-    computer: "computer-cards",
-}
+// const playerSides = {
+//     player1: "player-cards",
+//     computer: "computer-cards",
+// }
 
 // Enumeracao De Cartas
 const pathImages = "./src/assets/icons/";
@@ -34,7 +40,7 @@ const cardData = [
         type: "Paper",
         img: `${pathImages}dragon.png`,
         winOf:[1],
-        loseOf: [2],
+        loseOf:[2],
     },
     {
         id: 1,
@@ -42,7 +48,7 @@ const cardData = [
         type: "Rock",
         img: `${pathImages}magician.png`,
         winOf:[2],
-        loseOf: [0],
+        loseOf:[0],
     },
     {
         id: 2,
@@ -50,7 +56,7 @@ const cardData = [
         type: "Scissors",
         img: `${pathImages}exodia.png`,
         winOf:[0],
-        loseOf: [1],
+        loseOf:[1],
     },
 ]
 
@@ -67,7 +73,21 @@ async function createCardImage(IdCard, fieldSide){
     cardImage.classList.add("card");
     cardImage.classList.add("playable");
 
-    if (fieldSide === playerSides.player1) {
+
+    document.getElementById("player-field-card").addEventListener("mouseover", ()=>{
+        if (document.getElementById("player-field-card").getAttribute("data-id") !== "-1") {
+            drawSelectedCard(document.getElementById("player-field-card").getAttribute("data-id"));
+        }
+        
+    });
+
+    document.getElementById("computer-field-card").addEventListener("mouseover", ()=>{
+        if (document.getElementById("computer-field-card").getAttribute("data-id") !== "-1") {
+            drawSelectedCard(document.getElementById("computer-field-card").getAttribute("data-id"));
+        }
+    });
+
+    if (fieldSide === state.playerSides.player1 ) {
         cardImage.addEventListener("mouseover", ()=>{
             drawSelectedCard(IdCard);
         });
@@ -75,17 +95,22 @@ async function createCardImage(IdCard, fieldSide){
         cardImage.addEventListener("click", ()=>{
             //console.log(cardImage);
             //console.log(cardImage.classList.contains("playable") === true);
+
             if(cardImage.classList.contains("playable")){
-                for (let i = 0; i < document.getElementById("player-cards").getElementsByClassName("card").length; i++) {
-                    document.getElementById("player-cards").getElementsByClassName("card")[i].classList.add("playable");
-                }
+                let selected = document.getElementById("player-cards").querySelectorAll("img");
+                selected.forEach((img) => {img.classList.add("playable")});
+                // for (let i = 0; i < document.getElementById("player-cards").getElementsByClassName("card").length; i++) {
+                //     document.getElementById("player-cards").getElementsByClassName("card")[i].classList.add("playable");
+                // }
                 setCardsField(cardImage.getAttribute("data-id"));
                 cardImage.classList.remove("playable");
                 state.actions.init.style.display = "block";
+                state.actions.button.style.display = "none";
+                state.fieldCards.computer.src = "./src/assets/icons/card-front.png";
             } else {
                 cardImage.classList.add("playable");
                 //alert('Selecionada Already');
-                removeAllCardsImages();
+                clearPlayer();
             }
             
         });
@@ -93,17 +118,41 @@ async function createCardImage(IdCard, fieldSide){
     return cardImage;
 }
 
-async function removeAllCardsImages() {
+async function clearPlayer() {
     state.fieldCards.player.src = "./src/assets/icons/card-front.png";
     state.fieldCards.player.setAttribute("data-id", "");
     state.actions.init.style.display = "none";
 }
 
+async function clearField() {
+    state.fieldCards.player.src = "./src/assets/icons/card-front.png";
+    state.fieldCards.player.setAttribute("data-id", "");
+    state.fieldCards.computer.src = "./src/assets/icons/card-front.png";
+    state.fieldCards.computer.setAttribute("data-id", "");
+}
+
+async function removeAllCardsImages() {
+    // let {computerBOX, player1BOX} = state.playerSides;
+    // let imgElements = computerBOX.querySelectorAll("img");
+    // imgElements.forEach((img) => img.remove());
+}
+
+async function resetDuel() {
+    state.fieldCards.player.src = "./src/assets/icons/card-front.png";
+    state.fieldCards.computer.src = "./src/assets/icons/card-front.png";
+    state.fieldCards.player.setAttribute("data-id", "");
+    state.actions.init.style.display = "none";
+    state.actions.button.style.display = "none";
+}
+
 async function initDuel(){
+    state.actions.button.style.display = "block";
+    state.actions.init.style.display = "none";
+    //console.log(document.querySelector("#computer-cards").querySelectorAll("img"));
     let checkCards = document.getElementById("computer-cards").getElementsByClassName("playable");
     let computerCard = [];
 
-    //console.log(checkCards);
+    console.log(checkCards);
     for (let i = 0; i < checkCards.length; i++) {
         if(checkCards[i].style.display !== 'none'){
             computerCard[i] = checkCards[i];
@@ -126,6 +175,20 @@ async function initDuel(){
     }
     //console.log(state.fieldCards.player.getAttribute("data-id"));
     //console.log(state.fieldCards.computer.getAttribute("data-id"));
+    
+    //state.fieldCards.player.src = cardData[cardId].img;
+
+    document.getElementById("player-cards").classList.contains("playable");
+
+    checkCards = document.getElementById("player-cards").querySelectorAll("img");
+
+    checkCards.forEach((img) => {
+        if (!img.classList.contains("playable")) {
+            img.style.display = 'none';
+        }
+    });
+
+    await removeAllCardsImages();
 
     let duelResults = await checkDuelResults(state.fieldCards.player.getAttribute("data-id"), state.fieldCards.computer.getAttribute("data-id"));
 
@@ -133,8 +196,35 @@ async function initDuel(){
     await drawButton(duelResults);
 }
 
+async function updateScore() {
+    document.getElementById("score_points").innerText = `win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`;
+}
+
+async function drawButton(texto){
+    state.actions.button.innerText = texto;
+    console.log(texto);
+}
+
+async function checkDuelResults(playerCardId, computerCardId){
+    let duelResults = "Draw";
+    let playerCard = cardData[playerCardId];
+
+    if (playerCard.winOf.includes(parseInt(computerCardId))) {
+        duelResults = "Winner";
+        state.score.playerScore++;
+     } else if (playerCard.loseOf.includes(parseInt(computerCardId))) {
+         duelResults = "Lose";
+         state.score.computerScore++;
+     }
+
+    console.log(playerCard);
+    console.log(computerCardId);
+
+    console.log(duelResults);
+    return duelResults;
+}
+
 async function setCardsField(cardId){
-    await removeAllCardsImages();
 
     //let computerCardId = await getRandomCardId();
 
@@ -187,8 +277,8 @@ async function drawCards(cardNumbers, fieldSide){
 
 function inicialize() {
     //drawCards(5, "player");
-    drawCards(5, playerSides.player1);
-    drawCards(5, playerSides.computer);
+    drawCards(5, state.playerSides.player1);
+    drawCards(5, state.playerSides.computer);
 
     //playerSides.player1;
     //state.score.playerScore++;
